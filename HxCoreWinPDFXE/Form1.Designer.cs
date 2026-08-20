71425b9e7310e706ddcc9d2ad3d0a10ba3e8b3a8
jhpark <jhpark@htenc.co.kr>
2026-08-20 13:47:59 +0900

    환경 변경

--- HxCoreWinPDFXE/Form1.Designer.cs
+++ HxCoreWinPDFXE/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace HxCore.Win.PDFXE
+﻿
+namespace HxCore.Win.PDFXE
 {
     partial class Form1
     {
@@ -36,7 +37,7 @@
             // axPXV_Control1
             // 
             this.axPXV_Control1.Enabled = true;
-            this.axPXV_Control1.Location = new System.Drawing.Point(391, 100);
+            this.axPXV_Control1.Location = new System.Drawing.Point(113, 97);
             this.axPXV_Control1.Name = "axPXV_Control1";
             this.axPXV_Control1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axPXV_Control1.OcxState")));
             this.axPXV_Control1.Size = new System.Drawing.Size(192, 192);
